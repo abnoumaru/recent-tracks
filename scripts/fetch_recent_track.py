3a3f8e364d8128408beec1e01a1666cae8340c7a
Takaaki ABE <abnoumaru@gmail.com>
2024-09-20 08:55:13 +0900

    first commit

---
+++ scripts/fetch_recent_track.py
@@ -0,0 +1,28 @@
+import os
+import requests
+import json
+
+def fetch_json():
+    lastfm_user = os.getenv("LASTFM_USER")
+    api_key = os.getenv("API_KEY")
+    
+    if not lastfm_user or not api_key:
+        print("Error: LASTFM_USER or API_KEY environment variables are not set.")
+        return
+    
+    url = f"https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={lastfm_user}&api_key={api_key}&format=json&limit=1"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        with open('recent_track.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+        print("JSON data saved successfully to recent_track.json.")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    fetch_json()
